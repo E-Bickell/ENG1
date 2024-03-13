e0884419e655a69fa8ecbcfa0f2f0aaa7e055f0b
E-Bickell <148374096+E-Bickell@users.noreply.github.com>
2024-03-13 22:39:39 +0000

    Update ActivityTracker.java
    made ActivityTracker into a singleton class

--- code/core/src/com/mygdx/heslingtonhustle/ActivityTracker.java
+++ code/core/src/com/mygdx/heslingtonhustle/ActivityTracker.java
@@ -4,7 +4,7 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class ActivityTracker {
-    ActivityTracker tracker;
+    private ActivityTracker tracker;
     Dictionary<String, Integer> done= new Hashtable<>();
 
     private ActivityTracker(){
@@ -13,6 +13,12 @@ public class ActivityTracker {
         done.put("study",0);
         done.put("sleep",0);
 
+    }
+    public static ActivityTracker getActivityTracker{
+        if (tracker==null){
+            tracker= new ActivityTracker();
+        }
+        return tracker;
     }
     protected boolean addActivity(String type){
         done.put(type,done.get(type)+1);
